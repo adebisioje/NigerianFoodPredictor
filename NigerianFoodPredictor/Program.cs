22ca7213f7bbb99ae1b33c2f9a48547b9657814d
adebisioje <bisioje@gmail.com>
2017-06-01 12:58:12 -0400

    Addting predition

--- NigerianFoodPredictor/Program.cs
+++ NigerianFoodPredictor/Program.cs
@@ -6,6 +6,8 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace NigerianFoodPredictor
 {
@@ -40,6 +42,7 @@ namespace NigerianFoodPredictor
             string url = "https://southcentralus.api.cognitive.microsoft.com/customvision/v1.0/Prediction/4d5d3bfa-e477-452d-bef9-29afa749fbc1/image?iterationId=08aa5a19-83e8-4d10-a49c-5c39ac93354c";
 
             HttpResponseMessage response;
+            String response_json;
 
             // Request body. Try this sample with a locally stored image.
             byte[] byteData = GetImageAsByteArray(imageFilePath);
@@ -48,7 +51,31 @@ namespace NigerianFoodPredictor
             {
                 content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
                 response = await client.PostAsync(url, content);
+                response_json = await response.Content.ReadAsStringAsync();
                 Console.WriteLine(await response.Content.ReadAsStringAsync());
+            }
+
+            JObject results = JObject.Parse(response_json);
+            JArray predictions = (JArray)results.GetValue("Predictions");
+
+            if(predictions.Count == 0)
+            {
+                Console.WriteLine("I don't know what this is");
+            }
+            else
+            {
+                String suggestions = "";
+                for (int i=0; i < predictions.Count; i++)
+                {
+                    JObject prediction = (JObject)predictions[i];
+                    var probability = prediction.GetValue("Probability");
+                    if(Convert.ToDouble(probability) > 0.70)
+                    {
+                        var tag = prediction.GetValue("Tag");
+                        suggestions += tag + ", ";
+                    }
+                }
+                Console.WriteLine("I think this is " + suggestions.Remove(suggestions.Length -2 , 1));
             }
         }
     }
